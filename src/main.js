eae67673baa9cc0ceb621b0df9232e860ebf1264
jaoskipogi <tamlaylovestokwa@gmail.com>
2023-06-26 13:32:07 +0800

    added pinia-persisting plugin and axios

--- src/main.js
+++ src/main.js
@@ -7,10 +7,13 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import piniaPluginPersistedState from "pinia-plugin-persistedstate"
 
 const app = createApp(App)
 
-app.use(createPinia())
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedState)
+
 app.use(router)
 
 app.mount('#app')
--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,12 +1,39 @@
-import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import axios from 'axios';
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
+export const useDashboardStore = defineStore('dashboard', {
+  state: () => {
+    return { 
+       viewRegisterForm: false
+    }
+  },
+  getters: {
+    //for Summary Data
+    getViewRegisterForm: (state) => state.viewRegisterForm
+  },
+  actions: {
+    //NAMING CONVENTION SEMANTICS:
+    //fetch + function name
+    /* async getMerchantCoinSummary(token, params = {}){
+        const url = import.meta.env.VITE_BASE_BACKEND_URL + 'v1/merchants/qcoins/summary';
+        
+        let config = {
+          headers: {
+              'Content-Type': 'application/json;charset=UTF-8',
+              "Access-Control-Allow-Origin": "*",
+              'Authorization': 'Bearer ' + token
+          }
+        };
 
-  return { count, doubleCount, increment }
+        return await axios.post(url, params, config).then(function (data) {
+            return data;
+        }).catch(function (error) {
+            return error;
+        });
+    }, */
+  },
+
+  persist: {
+    storage: sessionStorage, // data in sessionStorage is cleared when the page session ends.
+  },
 })
